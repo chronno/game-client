923c5dc28b1cecd0e71423d2718f39b712a933c3
chronno <quinterosmanu@gmail.com>
2022-03-24 00:13:43 -0300

    cambio de skin y un poquito de ui sencilla para label y input text

--- src/main/java/com/chronno/client/screens/DefaultScreen.java
+++ src/main/java/com/chronno/client/screens/DefaultScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 
