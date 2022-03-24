3324e52f67381201abf1f0187faf2d808465bddd
chronno <quinterosmanu@gmail.com>
2022-03-24 00:12:15 -0300

    cambio de skin y un poquito de ui sencilla para label y input text

--- src/main/java/com/chronno/client/screens/DefaultScreen.java
+++ src/main/java/com/chronno/client/screens/DefaultScreen.java
@@ -3,9 +3,11 @@ package com.chronno.client.screens;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
 public class DefaultScreen extends ScreenAdapter {
 
@@ -15,13 +17,24 @@ public class DefaultScreen extends ScreenAdapter {
         this.engine = engine;
     }
 
+    private Stage stage;
+
 
     @Override
     public void show() {
         OrthographicCamera orthographicCamera = new OrthographicCamera(12, 8);
         try {
-            Skin skin = new Skin(Gdx.files.internal("/skins/clean-crispy-ui.json"));
-            TextButton button1 = new TextButton("Button 1", skin);
+            Skin skin = new Skin(Gdx.files.internal("skins/neon-ui.json"));
+            stage = new Stage();
+            Gdx.input.setInputProcessor(stage);
+            Label nameLabel = new Label("Name:", skin);
+            nameLabel.setY(620);
+            TextField nameInput = new TextField("", skin);
+            nameInput.setX(80);
+            nameInput.setY(615);
+            stage.addActor(nameLabel);
+            stage.addActor(nameInput);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -29,7 +42,11 @@ public class DefaultScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+
         engine.update(delta);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
@@ -37,5 +54,6 @@ public class DefaultScreen extends ScreenAdapter {
         engine.removeAllEntities();
         engine.removeAllSystems();
         engine.clearPools();
+        stage.dispose();
     }
 }
